4bb120c2d2ed3991ce6b395a74d04996dec59b21
mattdean <matthewallandean@gmail.com>
2021-02-16 18:25:28 -0700

    Hit dead end for this api

--- src/StatsContainer/StatsContainer.js
+++ src/StatsContainer/StatsContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './StatsContainer.css';
-import { getTeamsData, getGamesData, getGameData }from '../util';
+import { getGamesData, getGameData }from '../util';
 
 class StatsContainer extends React.Component {
   constructor() {
@@ -17,7 +17,6 @@ class StatsContainer extends React.Component {
   componentDidMount() {
     getGamesData()
     .then(response => {
-      console.log(response)
       this.setState({gamesData: response})
     })
     .catch(err => console.log(err))
@@ -29,52 +28,33 @@ class StatsContainer extends React.Component {
     // .catch(err => console.log(err))
   }
 
+  makeGames = () => {
+    return this.state.gamesData.data.map(game => {
+      return (
+        <article className="game">
+        <section classname="home">
+          <p>{game.home_team.id}</p>
+          <p>{game.home_team.abbreviation}</p>
+          <p>{game.home_team.abbreviation}</p>
+          <p>{game.home_team.abbreviation}</p>
+        </section>
+        <section classname="visitor">
+          <p>{game.visitor_team.id}</p>
+          <p>{game.visitor_team.abbreviation}</p>
+          <p>{game.visitor_team.abbreviation}</p>
+          <p>{game.visitor_team.abbreviation}</p>
+        </section>
+          
+        </article>
+      )
+    })
+  }
+
   render() {
     if (this.state.gamesData) {
       return (
         <main className="main">
-        <article className="game">
-          <section className="home">
-            <p>
-              {this.state.gamesData.api.games[0].gameId}
-            </p>
-            <p>
-              {this.state.gamesData.api.games[0].hTeam.fullName}
-            </p>
-            <p>
-              RECORD
-            </p>
-            <p>
-              AVG POINTS FOR
-            </p>
-            <p>
-              AVG POINTS AGAINST
-            </p>
-          </section>
-          <section className="away">
-            <p>
-              {this.state.gamesData.api.games[0].vTeam.fullName}
-            </p>
-            <p>
-              RECORD
-            </p>
-            <p>
-              AVG POINTS FOR
-            </p>
-            <p>
-              AVG POINTS AGAINST
-            </p>
-          </section>
-        </article>
-        <article className="game">
-          GAME
-        </article>
-        <article className="game">
-          GAME
-        </article>
-        <article className="game">
-          GAME
-        </article>
+        {this.makeGames()}
       </main>
     )
   } else {
--- src/util.js
+++ src/util.js
@@ -1,17 +1,3 @@
-export const getTeamsData = () => {
-  return fetch("https://api-nba-v1.p.rapidapi.com/teams/league/standard", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "f4bd5b032amsh639ee2a4f9fab83p16b39ajsnbefeb02eb78f",
-		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
-	}
-  })
-  .then(response => response.json())
-  .catch(err => {
-    throw new Error("Problem with fetch", err);
-  });
-}
-
 export const getGamesData = () => {
   return fetch("https://www.balldontlie.io/api/v1/games?dates[]=2021-02-17")
   .then(response => response.json())
@@ -20,16 +6,6 @@ export const getGamesData = () => {
   });
 }
 
-// export const getGameData = () => {
-//   return fetch("https://api-nba-v1.p.rapidapi.com/statistics/games/gameId/8608", {
-//     "method": "GET",
-//     "headers": {
-//       "x-rapidapi-key": "f4bd5b032amsh639ee2a4f9fab83p16b39ajsnbefeb02eb78f",
-//       "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
-//     }
-//   })
-//   .then(response => response.json())
-//   .catch(err => {
-//     throw new Error("Problem with fetch", err);
-//   });
-// }
+export const getGameData = () => {
+  return true
+}
